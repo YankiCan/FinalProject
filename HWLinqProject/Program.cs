9935b5796fa31c7316b3d2fad7b52a29bebb4b64
Yankı Can <yankican8@gmail.com>
2021-02-11 02:56:48 +0300

    Güncelleme

--- HWLinqProject/Program.cs
+++ HWLinqProject/Program.cs
@@ -18,60 +18,61 @@ namespace HWLinq_01_Project
             {
                 new Product{ProductId = 1, ProductName = "Acer Laptop", QuantityPerUnit = "32 GB Ram", UnitPrice = 10000, UnitsInStock = 5 },
                 new Product{ProductId = 2, ProductName = "Asus Laptop", QuantityPerUnit = "16 GB Ram", UnitPrice = 8000, UnitsInStock = 3 },
-                new Product{ProductId = 3, ProductName = "Hp Laptop", QuantityPerUnit = "8 GB Ram", UnitPrice = 6000, UnitsInStock = 2 },
+                new Product{ProductId = 3, ProductName = "Hp Laptop", QuantityPerUnit = "8 GB Ram", UnitPrice = 18000, UnitsInStock = 2 },
                 new Product{ProductId = 4, ProductName = "Samsung Telefon", QuantityPerUnit = "4 GB Ram", UnitPrice = 5000, UnitsInStock = 15 },
                 new Product{ProductId = 5, ProductName = "Apple Telefon", QuantityPerUnit = "32 GB Ram", UnitPrice = 8000, UnitsInStock = 0 },
-
             };
+
+
+            Console.WriteLine("Algoritmik--------");
 
             foreach (var product in products)
             {
-                if (product.UnitPrice > 5000 && product.UnitsInStock > 3) 
+                if (product.UnitPrice > 5000 && product.UnitsInStock > 3)
                 {
                     Console.WriteLine(product.ProductName);
                 }
+                
             }
 
-            Console.WriteLine("Linq--------------"); // Diğer yöntemi
+           
+            Console.WriteLine("Linq-------------------");
 
-            var result = products.Where(p => p.UnitPrice > 5000 && p.UnitsInStock > 3);    // var result yerine List<Product>
-            // Where - foreach sağlar
+            var result = products.Where(p => p.UnitPrice > 5000 && p.UnitsInStock > 3); // var result yerine List<Product> kullanılabilir - where foreach sağlar
 
             foreach (var product in result)
             {
                 Console.WriteLine(product.ProductName);
             }
 
-
-
             GetProducts(products);
-
-            static List<Product> GetProducts (List<Product> products)
-            {
-                List<Product> filteredProducts = new List<Product>();
-
-                foreach (var product in products)
-                {
-
-                    if (product.UnitPrice > 5000 && product.UnitsInStock > 3)
-                    {
-                        filteredProducts.Add(product);
-                    }
-                }
-
-                return filteredProducts;
-            }
-
-
-
-            // GetProductsLinq(products); ???
-            static List<Product> GetProductsLinq(List<Product> products)
-            {
-                return products.Where(product => product.UnitPrice > 5000 && product.UnitsInStock > 3).ToList();
-            }
         }
 
+       
+        static List<Product> GetProducts(List<Product> products)
+        {
+
+            List<Product> filteredProducts = new List<Product>();
+
+            foreach (var product in products)
+            {
+                if (product.UnitPrice > 5000 && product.UnitsInStock > 3)
+                {
+                    filteredProducts.Add(product);
+                }
+
+            }
+
+            return filteredProducts;
+        }
+
+
+        static List<Product> GetProductsLinq(List<Product> products) 
+        {
+            return products.Where(p => p.UnitPrice > 5000 && p.UnitsInStock > 3).ToList();
+        }
     }
+
 
     class Product
     {
